5d2bb0ac960616c1b35fb012d6d2128a2e56c572
mobileOneTrust <mobile@onetrust.com>
2022-09-28 20:31:32 -0400

    Deploy 202209.2.0

---
+++ Sources/OTPublishersHeadlessSDK.xcframework/ios-arm64/OTPublishersHeadlessSDK.framework/Headers/OTPublishersHeadlessSDK.h
@@ -0,0 +1,14 @@
+//
+//  OTPublishersHeadlessSDK.h
+//  OTPublishersHeadlessSDK
+//
+//  Created by OneTrust on 19/06/20.
+//  Copyright © 2020 OneTrust. All rights reserved.
+//
+
+#ifndef OTPublishersHeadlessSDK_h
+#define OTPublishersHeadlessSDK_h
+
+#include "TCF2Encoder.h"
+
+#endif /* OTPublishersHeadlessSDK_h */
